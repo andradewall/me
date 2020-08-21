f6a4b953a778412ad012818b5e691a5d6b6f7786
andradewall <andradewall0@gmail.com>
2020-08-20 22:05:38 -0300

    Changing animation order

--- js/modules/animation.js
+++ js/modules/animation.js
@@ -1,15 +1,29 @@
 export default function initAnimation() {
     const header = document.querySelector('.header');
-    const main = document.querySelector('.main');
+    const title = document.querySelector('.title');
+    const subtitle = document.querySelector('.subtitle');
+    const description = document.querySelector('.main-section__description');
+    const links = document.querySelector('.list');
     const footer = document.querySelector('.footer');
     
     header.classList.add('animationShowDown');
     
     setTimeout(() => {
-        main.classList.add('animationShowRight');
+        title.classList.add('animationShowRight');
+        setTimeout(() => {
+            subtitle.classList.add('animationShow');
+            setTimeout(() => {
+                description.classList.add('animationShow');
+                setTimeout(() => {
+                    links.classList.add('animationShow');
+                    setTimeout(() => {
+                        footer.classList.add('animationShow');
+                    }, 1500);
+                }, 500);
+            }, 1000);
+        }, 1000);
+
     }, 1300);
     
-    setTimeout(() => {
-        footer.classList.add('animationShow');
-    }, 2000);
+    
 }
